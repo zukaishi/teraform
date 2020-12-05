b6896aed2b2289dfb3a896090474f462bd1cb386
zukaishi <ishizuka215@gmail.com>
2020-12-05 16:10:15 +0900

    update

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "web1" {
-    ami           = "ami-f80e0596"
-    instance_type = "t2.micro"
-    monitoring    = true
-    tags {
-        Name = "web1"
-    }
+data aws_ssm_parameter amzn2_ami {
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
+
+resource aws_instance demo {
+  ami = data.aws_ssm_parameter.amzn2_ami.value
+  instance_type = "t2.micro"
 }
