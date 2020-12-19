78af5e6f7d22c07d492b88df294081f8253b2b33
zukaishi <ishizuka215@gmail.com>
2020-12-19 23:36:45 +0900

    update

--- workspace/main.tf
+++ workspace/main.tf
@@ -1,8 +1,8 @@
-data aws_ssm_parameter amzn2_ami {
-  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
-}
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 
-resource aws_instance demo {
-  ami = data.aws_ssm_parameter.amzn2_ami.value
-  instance_type = "t2.micro"
+  tags = {
+    Name = "workspace"
+  }
 }
