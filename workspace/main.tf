9709d6f1ae855680b36de2da38cd445dbebe1784
zukaishi <ishizuka215@gmail.com>
2020-12-20 21:19:11 +0900

    update

--- workspace/main.tf
+++ workspace/main.tf
@@ -6,3 +6,15 @@ resource "aws_vpc" "main" {
     Name = "workspace"
   }
 }
+
+# Subnet
+resource "aws_subnet" "public_1a" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  availability_zone = "ap-northeast-1a"
+
+  cidr_block        = "10.0.1.0/24"
+  tags = {
+    Name = "workspace-public-1a"
+  }
+}
