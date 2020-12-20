459dbb1bfb5ddd79d87eb5c1efdf0470ffa544e8
zukaishi <ishizuka215@gmail.com>
2020-12-20 21:23:07 +0900

    update

--- workspace/main.tf
+++ workspace/main.tf
@@ -1,7 +1,6 @@
 # VPC
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
-
   tags = {
     Name = "workspace"
   }
@@ -10,11 +9,55 @@ resource "aws_vpc" "main" {
 # Subnet
 resource "aws_subnet" "public_1a" {
   vpc_id = "${aws_vpc.main.id}"
-
   availability_zone = "ap-northeast-1a"
-
-  cidr_block        = "10.0.1.0/24"
+  cidr_block = "10.0.1.0/24"
   tags = {
     Name = "workspace-public-1a"
+  }
+}
+
+resource "aws_subnet" "public_1c" {
+  vpc_id = "${aws_vpc.main.id}"
+  availability_zone = "ap-northeast-1c"
+  cidr_block = "10.0.2.0/24"
+  tags = {
+    Name = "workspace-public-1c"
+  }
+}
+
+resource "aws_subnet" "public_1d" {
+  vpc_id = "${aws_vpc.main.id}"
+  availability_zone = "ap-northeast-1d"
+  cidr_block = "10.0.3.0/24"
+  tags = {
+    Name = "workspace-public-1d"
+  }
+}
+
+# Private Subnets
+resource "aws_subnet" "private_1a" {
+  vpc_id = "${aws_vpc.main.id}"
+  availability_zone = "ap-northeast-1a"
+  cidr_block = "10.0.10.0/24"
+  tags = {
+    Name = "workspace-private-1a"
+  }
+}
+
+resource "aws_subnet" "private_1c" {
+  vpc_id = "${aws_vpc.main.id}"
+  availability_zone = "ap-northeast-1c"
+  cidr_block        = "10.0.20.0/24"
+  tags = {
+    Name = "workspace-private-1c"
+  }
+}
+
+resource "aws_subnet" "private_1d" {
+  vpc_id = "${aws_vpc.main.id}"
+  availability_zone = "ap-northeast-1d"
+  cidr_block = "10.0.30.0/24"
+  tags = {
+    Name = "workspace-private-1d"
   }
 }
