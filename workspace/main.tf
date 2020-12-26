fd3828daa89ff04ab6013d977967ae09ab436cd0
zukaishi <ishizuka215@gmail.com>
2020-12-26 15:46:23 +0900

    update

--- workspace/main.tf
+++ workspace/main.tf
@@ -1,6 +1,65 @@
+# AMI
+data "aws_ami" "workspace" {
+  most_recent = true
+  owners = ["amazon"]
+
+  filter { 
+    name = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter { 
+    name = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "block-device-mapping.volume-type"
+    values = ["gp2"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
+
+# EC2
+resource "aws_instance" "workspace" {
+  ami                    = data.aws_ami.workspace.image_id
+  vpc_security_group_ids = [aws_security_group.workspace.id]
+  subnet_id              = aws_subnet.workspace.id
+  key_name               = aws_key_pair.workspace.id
+  instance_type          = "t2.micro"
+
+  tags = {
+    Name = "workspace"
+  }
+}
+
+# Elastic IP
+resource "aws_eip" "workspace" {
+  instance = aws_instance.workspace.id
+  vpc = true
+}
+
+# Key Pair
+resource "aws_key_pair" "workspace" {
+  key_name   = "workspace"
+  public_key = file("./workspace.pub")
+}
+
 # VPC
 resource "aws_vpc" "workspace" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
   tags = {
     Name = "workspace"
   }
@@ -11,14 +70,17 @@ resource "aws_subnet" "workspace" {
   vpc_id = "${aws_vpc.workspace.id}"
   availability_zone = "ap-northeast-1a"
   cidr_block = "10.0.1.0/24"
+
+  map_public_ip_on_launch = true
   tags = {
-    Name = "workspace-public-1a"
+    Name = "workspace"
   }
 }
 
 # Internet Gateway
 resource "aws_internet_gateway" "workspace" {
   vpc_id = "${aws_vpc.workspace.id}"
+
   tags = {
     Name = "workspace"
   }
@@ -82,62 +144,6 @@ resource "aws_security_group_rule" "out_all" {
   from_port         = 0
   to_port           = 0
   protocol          = "-1"
-}
-
-# AMI
-data "aws_ami" "workspace" {
-  most_recent = true
-  owners = ["amazon"]
-
-  filter { 
-    name = "architecture"
-    values = ["x86_64"]
-  }
-
-  filter { 
-    name = "root-device-type"
-    values = ["ebs"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  filter {
-    name   = "block-device-mapping.volume-type"
-    values = ["gp2"]
-  }
-
-  filter {
-    name   = "state"
-    values = ["available"]
-  }
-}
-
-# EC2
-resource "aws_instance" "workspace" {
-  ami                    = data.aws_ami.workspace.image_id
-  vpc_security_group_ids = [aws_security_group.workspace.id]
-  subnet_id              = aws_subnet.workspace.id
-  key_name               = aws_key_pair.workspace.id
-  instance_type          = "t2.micro"
-
-  tags = {
-    Name = "workspace"
-  }
-}
-
-# Elastic IP
-resource "aws_eip" "workspace" {
-  instance = aws_instance.workspace.id
-  vpc = true
-}
-
-# Key Pair
-resource "aws_key_pair" "workspace" {
-  key_name   = "workspace"
-  public_key = file("./workspace.pub")
 }
 
 # output
